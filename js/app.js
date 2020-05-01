cf09c50ded956b969f696d0926f0d0010b27119b
Danica Stanković <danica.stankovic.94@gmail.com>
2020-05-01 23:42:49 +0200

    js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let val;
+
+val = document;
+
+document.getElementById('ant_nedelje').innerText = 'Да смо на време издвојили жито од кукоља, сада не бисмо млатили празну сламу.';
+document.getElementById('afo_nedelje').innerText = 'Хоћу да се бавим бизнисом, али не могу. Првог дана би ме ухватили на делу!';
+
+document.getElementById('ant_nedelje').style.fontWeight = 900;
+document.getElementById('afo_nedelje').style.fontWeight = 900;
+
+console.log(val);
